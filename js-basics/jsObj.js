b5d9ce4404767d7797c4592159fb9c697b5ed1e1
AnirbanGhoshh <116192747+AnirbanGhoshh@users.noreply.github.com>
2026-01-24 15:20:45 +0530

    OBJSorting

---
+++ js-basics/jsObj.js
@@ -0,0 +1,14 @@
+let newThing = { a: 3, b: 1, c: 2 };
+let newArr = Object.entries(newThing).map(([x,y])=>{
+    return [x,y] = [y,x]; 
+});
+
+newArr.sort((x,y)=>{
+    return x[0] - y[0];
+});
+
+let newerArr = newArr.map(([x,y])=>{
+    return [x,y] = [y,x];
+});
+
+console.log(newerArr);
