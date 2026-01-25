8ccad8c14a0889769dbd2e19477dfa72f796c8fe
AnirbanGhoshh <116192747+AnirbanGhoshh@users.noreply.github.com>
2026-01-25 11:02:52 +0530

    Passed

--- js-built-ins/problems/medium/findDuplicates.js
+++ js-built-ins/problems/medium/findDuplicates.js
@@ -20,7 +20,9 @@
 
 
 function findDuplicates(arr) {
-  return arr.filter((ele, index) => arr.indexOf(ele) !== index);
+  return arr.filter((ele,idx)=>{
+    return arr.indexOf(ele) !== idx;
+  });
 }
 
 module.exports = findDuplicates;
