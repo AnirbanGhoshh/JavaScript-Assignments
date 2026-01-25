7427673f81d3861cac0461a99000daff52bd33ea
AnirbanGhoshh <116192747+AnirbanGhoshh@users.noreply.github.com>
2026-01-25 18:46:49 +0530

    passed

--- 01-js/medium/count-vowels.js
+++ 01-js/medium/count-vowels.js
@@ -7,6 +7,15 @@
 
 function countVowels(str) {
     // Your code here
+    let vowels = ['a','e','i','o','u'];
+    let countVowels=0;
+    let strArr = str.toLowerCase().split('');
+    for(let it of strArr){
+      if(vowels.includes(it)){
+        countVowels++;
+      }
+    }
+    return countVowels;
 }
 
 module.exports = countVowels;
