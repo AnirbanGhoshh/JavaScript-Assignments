01787e35ad9f9ec7150c74338a2d7b832705a4ee
AnirbanGhoshh <116192747+AnirbanGhoshh@users.noreply.github.com>
2026-01-25 19:03:08 +0530

    checked

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,14 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let sum = 0;
+    let start = Date.now();
+    for(let i=1 ; i <= n ; i++){
+        sum+=i;
+    }
+    let end = Date.now();
+
+    return end - start;
 }
+
+console.log(`time taken to execute is ${calculateTime(10000000000)} ms`);
